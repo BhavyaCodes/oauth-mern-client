77884466cf26d0218ad40d8c3f640040611ae726
Juggernau9 <xbhavyatomar@gmail.com>
2020-09-29 07:36:26 +0530

    profile shows user posts now

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,14 +3,28 @@ import axios from "axios";
 
 function Profile() {
   const [user, setUser] = useState(null);
+  const [userPosts, setUserPosts] = useState(null);
 
   useEffect(() => {
     const getUser = async () => {
       const res = await axios.get("/api/userinfo");
-      setUser(res.data.user);
+      const { imageUrl, name, posts } = res.data.user;
+      setUser({ imageUrl, name });
+      setUserPosts(posts);
     };
     getUser();
   }, []);
+
+  const renderPosts = () => {
+    if (userPosts) {
+      return userPosts.map((post) => (
+        <div key={post._id}>
+          <h1>{post.title}</h1>
+          <p>{post.body}</p>
+        </div>
+      ));
+    } else return null;
+  };
 
   return (
     <div>
@@ -18,6 +32,7 @@ function Profile() {
         <div>
           <img alt="profile pic" src={user.imageUrl} />
           <h1>{user.name}</h1>
+          {renderPosts()}
         </div>
       )}
     </div>
