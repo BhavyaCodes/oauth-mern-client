310910b687999e479eef2ac7fb4833880448b2ae
Juggernau9 <xbhavyatomar@gmail.com>
2020-09-29 06:21:11 +0530

    PostForm working

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,9 +1,35 @@
 import React from "react";
+import axios from "axios";
+
+import useInputState from "../hooks/useInputState";
 
 function PostForm() {
+  const [title, handleTitleChange, resetTitle] = useInputState("");
+  const [body, handleBodyChange, resetBody] = useInputState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await axios.post(
+      "/api/post",
+      {
+        title,
+        body,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: true,
+      }
+    );
+    resetTitle();
+    resetBody();
+  };
+
   return (
-    <form>
-      <textarea />
+    <form onSubmit={handleSubmit}>
+      <input value={title} onChange={handleTitleChange} />
+      <textarea value={body} onChange={handleBodyChange} />
       <button className="btn btn-primary" type="submit">
         Post
       </button>
